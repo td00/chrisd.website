11a62bb89f8b7436ce1ca1da3ca302a2e53bbb94
Thies Mueller <github@td00.de>
2018-02-21 12:51:06 +0100

    Multiple Changes Changed to css only added blind descriptions (CHANGE!) changed references.hook to references.image minor Changes @CSS Design

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,5 +1,6 @@
 // When the user clicks on <div>, open the popup
-function myFunction() {
+//deprecated, use only when opened @IE
+function lightbox() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
